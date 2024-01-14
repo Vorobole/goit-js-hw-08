eb3449559619bc8a1281e1b7270f8fd47930b3b4
Vorobole <vorobyov1903@gmail.com>
2024-01-14 15:08:59 +0200

    hw-08 done

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const inputName = document.querySelector('#name-input');
-const outputName = document.querySelector('#name-output');
-
-inputName.addEventListener('input', () => {
-  const inputValue = inputName.value.trim();
-  outputName.textContent = inputValue || 'Anonymous';
-});
